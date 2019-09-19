620385004f434bcbcf17db6a2e93ea7f00d2b5ef
Alex Harvey <alex.harvey@cmd.com.au>
2019-09-20 00:20:53 +1000

    Tweak example

--- examples/aggregator_account/main.tf
+++ examples/aggregator_account/main.tf
@@ -1,10 +1,15 @@
 variable "aggregator_account_id" {}
 variable "source_account_id" {}
 
-module "config" {
-  source = "../../"
-  is_aggregator = true
-  aggregator_account_id = var.aggregator_account_id
+variable "bucket_name" {
+  default = "config-bucket-1c5a1978-d138-4084-a3b4-fd4c403a89a0"
+}
+
+module "aggregator" {
+  source                    = "../../"
+  is_aggregator             = true
+  aggregator_account_id     = var.aggregator_account_id
   aggregator_account_region = "ap-southeast-2"
-  source_account_ids = [var.source_account_id]
+  source_account_ids        = [var.source_account_id]
+  bucket_name               = var.bucket_name
 }
