8a82a11cddfb7fe26a380a946017d203741096ea
Alex Harvey <alexharv074@gmail.com>
2019-09-21 21:15:32 +1000

    Add some altered ERB generation
    To also generate the erb/examples_README.erb. I'm about to revert
    this again but just in case I need it again later.

--- erb/docs.rb
+++ erb/docs.rb
@@ -1,4 +1,12 @@
-require 'erb'
-template = File.read('erb/README.erb')
-renderer = ERB.new(template, nil, '-')
-File.write('README.md', renderer.result())
+require "erb"
+
+docs = {
+  "erb/README.erb"          => "README.md",
+  "erb/examples_README.erb" => "examples/README.md",
+}
+
+docs.each do |erb,md|
+  template = File.read(erb)
+  renderer = ERB.new(template, nil, "-")
+  File.write(md, renderer.result())
+end
